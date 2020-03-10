56b330712c05a56b5412470353fb3d93f9c90ac7
xfil <lfilipevsl@gmail.com>
2020-03-10 10:19:32 +0000

    commit006

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pubdate = models.DateTimeField()
+    image = models.ImageField(upload_to='images/blog/')
+    body = models.TextField()
+# Create a blog models
+# Title
+#Publication Date
+#body
+#image
+
+#Add the blog app to the settings
+
+#Create a migrations
+
+#migrate
+
+#add to the Admin
