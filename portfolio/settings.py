f728178e291c6460b004df85724bde01bda89448
Filipe <lfilipe@gmail.com>
2020-03-10 09:04:47 +0000

    commit003
    further commitments!

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '111',
+        'HOST':'localhost',
     }
 }
 
